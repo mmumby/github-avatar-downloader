e52c9eb9b1c997096ac268efc262cb70bfaffcbf
Meredith <meredith.mumby@outlook.com>
2017-07-04 01:03:12 +0000

    created function to get repp contributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,39 @@
-
-var request = require('request');
+const request = require('request');
+const fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRequestOptions() {
+  return {
+    uri: 'https://api.github.com/repos/jquery/jquery/contributors',
+    headers: {
+      'User-Agent': 'mmumby'
+    },
+    // saved access token to the environment
+    qs: {
+      access_token: process.env.GITHUB_ACCESS_TOKEN
+    }
+  }
+}
+
+//function to request url
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  request(getRequestOptions(), function (error, response, body) {
+    try {
+      const data = JSON.parse(body);
+      console.log('Just called the callback');
+
+      cb(data);
+
+    } catch (err) {
+      console.log('failed to parse content body')
+    }
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log('the callback ran');
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
